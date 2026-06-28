bf81f955d0f2f80a43f163b191861546a4218b54
Aiden Murphy <aidenmurphy@macbook-pro.lan>
2026-06-28 14:55:55 -0400

    window opens pygame is working perfectly

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame 
+
+running = True
+FPS = 60
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800, 600))
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+    
+    clock.tick(FPS)
