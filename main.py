8e07560d04cbdf4abde206423d40ddf53ae0b066
Aiden Murphy <aidenmurphy@macbook-pro.lan>
2026-06-28 15:03:38 -0400

    drawing particles from list added

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ running = True
 FPS = 60
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((800, 600))
+red = (255,0,0)
+particles = []
+def add_particle(x,y):
+    particles.append([x,y])
+add_particle(400,300)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     pygame.display.flip()
-    
+    for p in particles:
+        particle = pygame.draw.circle(screen,red,(p[0],p[1]),10)
     clock.tick(FPS)
