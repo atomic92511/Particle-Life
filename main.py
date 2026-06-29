e47681ec8af74dae079e49326f2c0d3427b31e5e
Aiden Murphy <aidenmurphy@Aidens-MacBook-Pro.local>
2026-06-29 12:39:56 -0400

    second particle type im so happy I fixed the glitching

--- main.py
+++ main.py
@@ -13,22 +13,30 @@ particles = []
 
 effect_rad = 100
 part_count = 250
-force_sim_speed = 0.05
+force_sim_speed = 2
 particle_rad = 5
+friction = 0.8
 
-def add_particle(x,y):
-    particles.append([x,y,0,0])
+def add_particle(x,y,color):
+    particles.append([x,y,0,0,color])
 def get_dist(p1,p2):
     return math.dist(p1,p2)
 for i in range(part_count):
-    add_particle(random.randint(0,800),random.randint(0,600))
+    color = random.randint(0,1)
+    if color == 0:
+        add_particle(random.randint(0,800),random.randint(0,600),"red")
+    elif color == 1:
+            add_particle(random.randint(0,800),random.randint(0,600),"blue")
 while running:
     mx,my = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     if pygame.mouse.get_pressed()[0]:
-        add_particle(mx,my)
+        add_particle(mx,my,"blue")
+    for p in particles:
+        p[2] = 0
+        p[3] = 0
     for i in range(len(particles)):
         for j in range(i + 1,len(particles)):
             part1 = particles[i]
@@ -38,21 +46,42 @@ while running:
                 continue
             dx = part2[0] - part1[0]
             dy = part2[1] - part1[1]
+            nx = dx / dist
+            ny = dy / dist
             if dist < effect_rad and not dist < particle_rad + 10:
-                part1[2] = (part2[0] - part1[0]) * force_sim_speed
-                part1[3] = (part2[1] - part1[1]) * force_sim_speed
-                part2[2] = (part1[0] - part2[0]) * force_sim_speed
-                part2[3] = (part1[1] - part2[1]) * force_sim_speed
+                if part1[4] == "red":
+                    if part2[4] != "blue":
+                        part1[2] = nx * force_sim_speed
+                        part1[3] = ny * force_sim_speed
+                        part2[2] = nx * force_sim_speed
+                        part2[3] = ny * force_sim_speed
+                    elif part2[4] == "blue":
+                        part1[2] = nx * force_sim_speed
+                        part1[3] = ny * force_sim_speed
+                        part2[2] = nx * force_sim_speed
+                        part2[3] = ny * force_sim_speed
+                elif part1[4] == "blue":
+                        if part2[4] != "blue":
+                            part1[2] = -nx * force_sim_speed
+                            part1[3] = -ny * force_sim_speed
+                            part2[2] = -nx * force_sim_speed
+                            part2[3] = -ny * force_sim_speed
+                        if part2[4] == "blue":
+                            part1[2] = nx * force_sim_speed
+                            part1[3] = ny * force_sim_speed
+                            part2[2] = nx * force_sim_speed
+                            part2[3] = nx * force_sim_speed
+                                        
             elif dist < effect_rad and dist < particle_rad + 10:
-                part1[2] = (part1[0] - part2[0]) * force_sim_speed
-                part1[3] = (part1[1] - part2[1]) * force_sim_speed
-                part2[2] = (part2[0] - part1[0]) * force_sim_speed
-                part2[3] = (part2[1] - part1[1]) * force_sim_speed
-                min_dist = (particle_rad * 2) + 2
+                part1[2] = nx * force_sim_speed
+                part1[3] = ny * force_sim_speed
+                part2[2] = nx * force_sim_speed
+                part2[3] = ny * force_sim_speed
+                min_dist = (particle_rad * 2) - 1
                 if dist < min_dist:
                     overlap = min_dist - dist
-                    push_x = (dx / dist) * overlap * 0.5
-                    push_y = (dy / dist) * overlap * 0.5
+                    push_x = (dx / dist) * overlap * 0.9
+                    push_y = (dy / dist) * overlap * 0.9
                     
                     part1[0] -= push_x
                     part1[1] -= push_y
@@ -61,16 +90,21 @@ while running:
     for p in particles:
         p[0] += p[2]
         p[1] += p[3]
+        p[2] *= friction
+        p[3] *= friction
         if p[0] > 800:
-            p[0] = 800
-        if p[0] < 0:
             p[0] = 0
+        if p[0] < 0:
+            p[0] = 800
         if p[1] < 0:
-            p[1] = 0
-        if p[1] > 600:
             p[1] = 600
-    pygame.display.flip()
+        if p[1] > 600:
+            p[1] = 0
     screen.fill((0,0,0))
     for p in particles:
-        particle = pygame.draw.circle(screen,red,(p[0],p[1]),particle_rad)
+        if p[4] == "red":
+            particle = pygame.draw.circle(screen,red,(p[0],p[1]),particle_rad)
+        elif p[4] == "blue":
+            particle = pygame.draw.circle(screen,blue,(p[0],p[1]),particle_rad)
+    pygame.display.flip()
     clock.tick(FPS)
